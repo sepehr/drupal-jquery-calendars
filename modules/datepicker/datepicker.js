2a9694b5d9df5c5bc91c54ca03245c1ea39e87d4
Sepehr Lajevardi <me@sepehr.ws>
2010-12-26 13:53:04 +0330

    Added datapicker JS behavior.

---
+++ modules/datepicker/datepicker.js
@@ -0,0 +1,26 @@
+// $Id$
+
+/**
+ * @file
+ * Contains Datepicker Form Element JS behavior.
+ */
+
+/**
+ * Datepicker JS behavior that initialized the jQuery World Calendar
+ * Datepicker foreach object in Drupal.settings.datepicker.
+ */
+Drupal.behaviors.datepicker = function(context) {
+  for (var id in Drupal.settings.datepicker) {
+    var datepicker = Drupal.settings.datepicker[id];
+
+    // Initialize the Calendar.
+    datepicker.options.calendar = $.calendars.instance(datepicker.calendar, datepicker.locale);
+
+    // The command object. Eval is Evil, I know!
+    // datepicker.options.commands = eval(datepicker.calendar.commands);
+
+    // And initialize the Datepicker.
+    $(context).find('#' + id).calendarsPicker(datepicker.options);
+  }
+};
+
