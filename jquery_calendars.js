3ffdf02762636f021475d2b1248fb16b749f797b
Sepehr Lajevardi <me@sepehr.ws>
2011-07-23 01:27:25 +0430

    Updated demonstration JS behavior to be compatible with D7 JS API.

--- jquery_calendars.js
+++ jquery_calendars.js
@@ -3,40 +3,43 @@
  * Contains a jQuery World Calendars Datepicker demo javascript code.
  */
 
-/**
- * jQuery Calendars demo initializer.
- */
-Drupal.behaviors.jqueryCalendarsDemo = function(context) {
-  var $picker = $(context).find('#datepicker-demo'),
-      $calendars = $(context).find('#datepicker-demo-calendars');
+(function($) {
+  /**
+   * Attaches jQuery World Calendars demonstration behavior.
+   */
+  Drupal.behaviors.jqueryCalendarsDemo = {
+    attach: function(context) {
+      $picker = $('#datepicker-demo', context);
+      $calendars = $('#datepicker-demo-calendars', context);
 
-  // Initialize the library.
-  $picker.calendarsPicker({
-    monthsToShow: [2, 3],
-    showOtherMonths: false,
-    calendar: $.calendars.instance($calendars.val()),
-    onShow: $.calendars.picker.showStatus,
-    renderer: $.calendars.picker.weekOfYearRenderer,
-    onSelect: function(date) {
-      alert(Drupal.t('You picked @date.',
-        {'@date': date[0].formatDate()}
-      ));
-    },
-  });
+      // Initialize the library.
+      $picker.calendarsPicker({
+        monthsToShow: [2, 3],
+        showOtherMonths: false,
+        calendar: $.calendars.instance($calendars.val()),
+        onShow: $.calendars.picker.showStatus,
+        renderer: $.calendars.picker.weekOfYearRenderer,
+        onSelect: function(date) {
+          alert(Drupal.t('You picked @date.',
+            {'@date': date[0].formatDate()}
+          ));
+        }
+      });
 
-  // Bind onchange handler.
-  $calendars.bind('change', function() {
-    try {
-      var calendar = $.calendars.instance($calendars.val());
-      $picker
-        .calendarsPicker('option', {
-          calendar: calendar,
-        })
-        .calendarsPicker('showMonth');
+      // Bind onchange handler.
+      $calendars.bind('change', function() {
+        try {
+          var calendar = $.calendars.instance($calendars.val());
+          $picker
+            .calendarsPicker('option', {
+              calendar: calendar
+            })
+            .calendarsPicker('showMonth');
+        }
+        catch (e) {
+          alert(e);
+        }
+      });
     }
-    catch (e) {
-      alert(e);
-    }
-  });
-};
-
+  };
+})(jQuery);
